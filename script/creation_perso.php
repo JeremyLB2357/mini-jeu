31dec930dfee2aea6ccab7c8101d64f902b97d08
JLBportable <jeremy.lebosser@gmail.com>
2021-07-07 17:46:55 +0200

    build: connexion à la BDD

--- script/creation_perso.php
+++ script/creation_perso.php
@@ -1,8 +1,21 @@
 <?php
 
-echo 'test </br>';
 require './classes/personnage.php';
 
 $perso = new Personnage(100, 5, 5, 50);
 
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=mini-jeu_php;charset=utf8', 'root', '');
+}
+catch (Exception $e){
+    die('Erreur : ' . $e->getMessage());
+}
+
+$reponse = $bdd->query('SELECT * FROM personnages');
+
+while ($data = $reponse->fetch())
+{
+    echo 'nom : ' . $data['nom'];
+    echo 'niveau : ' . $data['niveau'];
+}
 ?>
