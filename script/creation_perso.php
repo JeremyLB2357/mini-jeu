fd7b27b22a716b91672988a3461ddbd9b1771aa3
JLBportable <jeremy.lebosser@gmail.com>
2021-07-07 19:59:04 +0200

    [todo]: champ de bataille

--- script/creation_perso.php
+++ script/creation_perso.php
@@ -2,12 +2,16 @@
 
 require './classes/personnage.php';
 
+$champ_bataille = new champ_bataille();
+$champ_bataille->retrieve_character();
+$champ_bataille->fight();
+
 $perso = new Personnage(100, 5, 5, 50);
 
 try {
     $bdd = new PDO('mysql:host=localhost;dbname=mini-jeu_php;charset=utf8', 'root', '');
 }
-catch (Exception $e){
+catch (PDOException $e){
     die('Erreur : ' . $e->getMessage());
 }
 
@@ -18,4 +22,8 @@ while ($data = $reponse->fetch())
     echo 'nom : ' . $data['nom'];
     echo 'niveau : ' . $data['niveau'];
 }
-?>
+$reponse = $bdd->query('SELECT * FROM personnages');
+while ($data = $reponse->fetch(PDO::FETCH_OBJ))
+{
+    var_dump($data);
+}
